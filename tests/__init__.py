a5181797c85073940839370fa042ac7a0f3c1504
Borja González Seoane <dev@glezseoane.com>
2019-06-30 11:35:39 +0200

    [test] Implement test suite
    Complete testing of library performance.

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+"""monoshape test suit"""
+
+__author__ = 'Borja González Seoane'
+__copyright__ = 'Copyright 2019, Borja González Seoane'
+__credits__ = 'Borja González Seoane'
+__license__ = 'LICENSE'
+__version__ = '0.1dev0'
+__maintainer__ = 'Borja González Seoane'
+__email__ = 'dev@glezseoane.com'
+__status__ = 'Development'
